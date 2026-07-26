34f50d72517ab2c7175ded75d07089fcd8417023
Sagi Bachar Adler <sagienator@gmail.com>
2026-07-26 20:22:32 +0300

    now it's fixed!

--- tests/test_main.py
+++ tests/test_main.py
@@ -29,7 +29,7 @@ def test_root():
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.json()["status"] == "broken"
+    assert response.json()["status"] == "ok"
 
 
 # --- Test 3: Get students – empty list ---
